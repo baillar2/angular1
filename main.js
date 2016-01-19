ec403e508e412099b5d589ee5ed467689d3218f7
Student <student@RU-002.local>
2016-01-19 16:12:11 -0700

    finished one

---
+++ main.js
@@ -0,0 +1,18 @@
+// TOP LEVEL COMPONENT - Module
+
+angular.module('moduleOne', []);
+
+// REGISTER A CONTROLLER TO OUR MODULE
+
+angular
+	.module('moduleOne')
+		.controller('controllerOne', ['$scope', function($scope){
+		
+		$scope.click = function(){
+			console.log("you clicked a button")
+		}
+
+
+
+
+		}])
